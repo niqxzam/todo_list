91e4744f5b42194ffae8d7543a48272c9e04fb79
niqxzam <shah_nidz@yahoo.com>
2018-05-29 14:28:38 +0800

    Signup form works

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+gem 'autoprefixer-rails'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
@@ -59,3 +60,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'bootstrap-sass', '~> 3.2.0'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,22 @@ class UsersController < ApplicationController
  
  #GET '/'
  def new
+  @user = User.new
  end
 
  #POST '/'
  def create
+  @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to your personal To-Do list app!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+ end
+
+ def show
+  @user = User.find(params[:id])
  end
 
  def hash_password(password)
@@ -15,4 +27,11 @@ class UsersController < ApplicationController
  def test_password(password, hash)
   BCrypt::Password.new(hash) == password
  end
+
+ private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :phone)
+  end
+
 end
