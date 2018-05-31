21c7afef42a7157e8cbafdf25dbaefe388072817
niqxzam <shah_nidz@yahoo.com>
2018-05-31 19:42:41 +0800

    DOne

--- config/initializers/twitter.rb
+++
@@ -1,8 +0,0 @@
-require 'twitter'
- 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_API_Key"]
-  config.consumer_secret     = ENV["TWITTER_API_Secret"]
-  config.access_token        = ENV["Twitter_Access_Token"]
-  config.access_token_secret = ENV["Twitter_Access_Token_Secret"]
-end
